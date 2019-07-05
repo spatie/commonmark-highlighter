cf3a87a762100bd474c0bbe31ac48e0f32979376
Ruben Van Assche <ruben@spatie.be>
2019-07-05 15:26:13 +0200

    Update to commonmark 1.0

--- src/FencedCodeRenderer.php
+++ src/FencedCodeRenderer.php
@@ -2,25 +2,30 @@
 
 namespace Spatie\CommonMarkHighlighter;
 
+use League\CommonMark\Block\Renderer\BlockRendererInterface;
 use League\CommonMark\Util\Xml;
 use League\CommonMark\Block\Element\FencedCode;
 use League\CommonMark\ElementRendererInterface;
 use League\CommonMark\Block\Element\AbstractBlock;
 use League\CommonMark\Block\Renderer\FencedCodeRenderer as BaseFencedCodeRenderer;
 
-class FencedCodeRenderer extends BaseFencedCodeRenderer
+class FencedCodeRenderer implements BlockRendererInterface
 {
     /** @var \Spatie\CommonMarkHighlighter\CodeBlockHighlighter */
     protected $highlighter;
 
+    /** @var \League\CommonMark\Block\Renderer\FencedCodeRenderer */
+    protected $baseRenderer;
+
     public function __construct(array $autodetectLanguages = [])
     {
         $this->highlighter = new CodeBlockHighlighter($autodetectLanguages);
+        $this->baseRenderer = new BaseFencedCodeRenderer();
     }
 
     public function render(AbstractBlock $block, ElementRendererInterface $htmlRenderer, $inTightList = false)
     {
-        $element = parent::render($block, $htmlRenderer, $inTightList);
+        $element = $this->baseRenderer->render($block, $htmlRenderer, $inTightList);
 
         $element->setContents(
             $this->highlighter->highlight(
--- src/IndentedCodeRenderer.php
+++ src/IndentedCodeRenderer.php
@@ -2,23 +2,28 @@
 
 namespace Spatie\CommonMarkHighlighter;
 
+use League\CommonMark\Block\Renderer\BlockRendererInterface;
 use League\CommonMark\ElementRendererInterface;
 use League\CommonMark\Block\Element\AbstractBlock;
 use League\CommonMark\Block\Renderer\IndentedCodeRenderer as BaseIndentedCodeRenderer;
 
-class IndentedCodeRenderer extends BaseIndentedCodeRenderer
+class IndentedCodeRenderer implements BlockRendererInterface
 {
     /** @var \Spatie\CommonMarkHighlighter\CodeBlockHighlighter */
     protected $highlighter;
 
+    /** @var \League\CommonMark\Block\Renderer\IndentedCodeRenderer */
+    protected $baseRenderer;
+
     public function __construct(array $autodetectLanguages = [])
     {
         $this->highlighter = new CodeBlockHighlighter($autodetectLanguages);
+        $this->baseRenderer = new BaseIndentedCodeRenderer();
     }
 
     public function render(AbstractBlock $block, ElementRendererInterface $htmlRenderer, $inTightList = false)
     {
-        $element = parent::render($block, $htmlRenderer, $inTightList);
+        $element = $this->baseRenderer->render($block, $htmlRenderer, $inTightList);
 
         $element->setContents(
             $this->highlighter->highlight($element->getContents())
